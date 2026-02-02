1576def336eace99b5bf1d067bd59858e5e7a8a4
Paul Hernandez <phernandez8@ggc.edu>
2026-02-01 19:04:20 -0500

    Added a screenshot of branch insight, and did a exercise on 07-beer.sh

--- 07-beer.sh
+++ 07-beer.sh
@@ -23,6 +23,53 @@ done
 # exercise: implement another counting song (such as 12 days of Christmas) 
 # using loops and if statements.
 
+echo "How many days should we sing about?"
+read day
+
+current=$day
+
+while [ $current -ge 1 ]; do
+	echo "On the $current day of our celebration, we enjoyed: "
+if [ $current -ge 12 ]; then
+   echo " - Twelve lanterns glowing"
+fi
+if [ $current -ge 11 ]; then
+   echo " - Eleven crystals shining"
+fi
+if [ $current -ge 10 ]; then
+   echo " - Ten banners waving"
+fi
+if [ $current -ge 9 ]; then
+   echo " - Nine bells ringing"
+fi
+if [ $current -ge 8 ]; then
+   echo " - Eight stars a‑twinkling"
+fi
+if [ $current -ge 7 ]; then
+   echo " - Seven candles burning"
+fi
+if [ $current -ge 6 ]; then
+   echo " - Six ribbons fluttering"
+fi
+if [ $current -ge 5 ]; then
+   echo " - Five golden stones"
+fi
+if [ $current -ge 4 ]; then
+  echo " - Four silver charms"
+fi
+if [ $current -ge 3 ]; then
+   echo " - Three bright feathers"
+fi
+if [ $current -ge 2 ]; then
+   echo " - Two wooden carvings"
+fi
+
+echo " - One shining star"
+echo ""
+
+((current = current - 1))
+done
+
 echo "Now we sing about monkeys jumping on the bed"
 echo "How many monkeys?"
 read count
